2d61c9d8c21e918c56968a60c6252531e23faf7c
rupgo <rubenbenarroch@gmail.com>
2026-01-03 21:59:43 +0100

    Initial Commit

---
+++ Solofoodies/SolofoodiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolofoodiesApp.swift
+//  Solofoodies
+//
+//  Created by Ruben Benarroch on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SolofoodiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SolofoodiesTests/SolofoodiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SolofoodiesTests.swift
+//  SolofoodiesTests
+//
+//  Created by Ruben Benarroch on 3/1/26.
+//
+
+import Testing
+@testable import Solofoodies
+
+struct SolofoodiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
